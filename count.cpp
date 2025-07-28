12e9f0a2d800d90ba4b77f842ded2dc2d35ed429
Ella Jozinovic <ellajozi01@gmail.com>
2025-07-28 10:53:33 +0930

    Modify count function to count number of even numbers, not sum

--- count.cpp
+++ count.cpp
@@ -4,7 +4,7 @@ int count(int array[], int n) {
     int total = 0;
     for (int i = 0; i < n; i++) {
         if (array[i] % 2 == 0) {
-            total += array[i];
+            total ++;
         }
     }
 
