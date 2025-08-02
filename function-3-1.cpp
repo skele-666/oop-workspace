b3f218bda8f8b8383c206fa7a5432baee5077e4d
Ella Jozinovic <ellajozi01@gmail.com>
2025-08-02 13:12:02 +0930

    Did not realise I had to check the array was ascending...

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -3,8 +3,16 @@ bool is_fanarray(int array[], int n) {
         return false;
     }
 
+    // Check if palindrome
     for (int i = 0; i < n / 2; i++) {
         if (array[i] != array[n - 1 - i]) {
+            return false;
+        }
+    }
+
+    // Check if elements up to middle are ascending
+    for (int i = 0; i < (n - 1) / 2; i++) {
+        if (array[i] >= array[i + 1]) {
             return false;
         }
     }
