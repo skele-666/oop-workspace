32df91cde0735f3a191146ae1e175ccb0fd75dca
Ella Jozinovic <ellajozi01@gmail.com>
2025-08-05 10:51:53 +0930

    Finish transform exercise

--- transform.cpp
+++ transform.cpp
@@ -1,12 +1,18 @@
-char transfrom(int n) {
-    int binary[] = {};
-    int i = 0;
+#include <string>
+
+std::string transform(int n) {
+    if (n < 1) {
+        return "0";
+    }
+
+    std::string result = "";
 
     while (n > 0) {
         int quotient = n / 2; // Will be an int because both operands are ints
-        int remainder = (quotient * 2) - n;
-        binary[i] = remainder;
-        i++;
-
+        int remainder = n - (quotient * 2);
+        result = std::to_string(remainder) + result; // To reverse string by prepending each new result before the old one
+        n = quotient;
     }
+
+    return result;
 }
