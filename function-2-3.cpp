ce6586f5bb46fcfe902eea08859f834a5d9388ee
Ella Jozinovic <ellajozi01@gmail.com>
2025-08-07 13:33:31 +0930

    Formatting

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,38 +1,48 @@
-bool is_palindrome(int integers[], int length) {
-    if (length <= 0) {
-        return false;
-    }
+bool is_palindrome(int integers[], int length)
+{
+	if (length <= 0)
+	{
+		return false;
+	}
 
-    for (int i = 0; i < length / 2; i++) {
-        if (integers[i] != integers[length - 1 - i]) {
-            return false;
-        }
-    }
+	for (int i = 0; i < length / 2; i++)
+	{
+		if (integers[i] != integers[length - 1 - i])
+		{
+			return false;
+		}
+	}
 
-    return true;
+	return true;
 }
 
-int sum_array_elements(int integers[], int length) {
-	if (!is_palindrome(integers, length)) {
+int sum_array_elements(int integers[], int length)
+{
+	if (!is_palindrome(integers, length))
+	{
 		return -1;
 	}
 
 	int sum = 0;
-	for (int i = 0; i < length; i++) {
+	for (int i = 0; i < length; i++)
+	{
 		sum += integers[i];
 	}
 
 	return sum;
 }
 
-int sum_if_palindrome(int integers[], int length) {
-    if (length <= 0) {
-        return -1;
-    }
+int sum_if_palindrome(int integers[], int length)
+{
+	if (length <= 0)
+	{
+		return -1;
+	}
 
-    if (!is_palindrome(integers, length)) {
-        return -2;
-    }
+	if (!is_palindrome(integers, length))
+	{
+		return -2;
+	}
 
-    return sum_array_elements(integers, length);
+	return sum_array_elements(integers, length);
 }
