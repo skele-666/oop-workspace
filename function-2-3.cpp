7cef5e49e2445cc8f8f4aa71d0f3ebcde15e773e
Ella Jozinovic <ellajozi01@gmail.com>
2025-08-07 13:28:22 +0930

    Finish 2-3

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,11 +1,38 @@
 bool is_palindrome(int integers[], int length) {
+    if (length <= 0) {
+        return false;
+    }
 
-}
+    for (int i = 0; i < length / 2; i++) {
+        if (integers[i] != integers[length - 1 - i]) {
+            return false;
+        }
+    }
 
-int sum_if_palindrome(int integers[], int length) {
-
+    return true;
 }
 
 int sum_array_elements(int integers[], int length) {
+	if (!is_palindrome(integers, length)) {
+		return -1;
+	}
 
+	int sum = 0;
+	for (int i = 0; i < length; i++) {
+		sum += integers[i];
+	}
+
+	return sum;
+}
+
+int sum_if_palindrome(int integers[], int length) {
+    if (length <= 0) {
+        return -1;
+    }
+
+    if (!is_palindrome(integers, length)) {
+        return -2;
+    }
+
+    return sum_array_elements(integers, length);
 }
---
+++ main-2-3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+extern int sum_if_palindrome(int integers[], int length);
+
+int main() {
+    // Test 1: Palindrome [1, 2, 3, 2, 1] -> sum = 9
+    int test1[] = {1, 2, 3, 2, 1};
+    cout << "Expected: 9\nActual:   " << sum_if_palindrome(test1, 5) << endl;
+
+    // Test 2: Not a palindrome [1, 2, 3, 4, 5] -> return -2
+    int test2[] = {1, 2, 3, 4, 5};
+    cout << "Expected: -2\nActual:   " << sum_if_palindrome(test2, 5) << endl;
+
+    // Test 3: Length 0 -> return -1
+    int test3[] = {};
+    cout << "Expected: -1\nActual:   " << sum_if_palindrome(test3, 0) << endl;
+
+    return 0;
+}
