86ce1e1dcfd1b3c831a69d850acd09460c56de3f
Ella Jozinovic <ellajozi01@gmail.com>
2025-08-11 11:17:16 +0930

    Fix bug

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ int main() {
 	// Part 8
 	double* arr6 = dynamicArray(6);
 	printArray(arr6, 6);
-	cout << "Max value in array: " << arrayMax(arr4, 9) << endl;
+	cout << "Max value in array: " << arrayMax(arr6, 9) << endl;
 	delete[] arr6;
 
 	return 0;
